0693f1fccfdedd43392e1993caf912db0bd0216b
岡浩平 <cafegolazo@gmail.com>
2025-08-19 11:09:26 +0000

    ログインができなくなりました。

---
+++ src/app/(app)/memories/[memoryId]/layout.tsx
@@ -0,0 +1,8 @@
+// src/app/(app)/memories/[memoryId]/layout.tsx
+export default function MemoryIdLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
---
+++ src/app/(app)/memories/new/layout.tsx
@@ -0,0 +1,8 @@
+// src/app/(app)/memories/new/layout.tsx
+export default function MemoriesLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <>{children}</>;
+}
