d2e657113527c5df6cd9687038e712361da7734b
岡浩平 <cafegolazo@gmail.com>
2025-08-26 03:02:33 +0000

    firebase deploy
    === Deploying to 'memorylink-cms'...
    
    i  deploying fire

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
   parserOptions: {
     project: ["tsconfig.json", "tsconfig.dev.json"],
     sourceType: "module",
+    tsconfigRootDir: __dirname, // Add this line
   },
   ignorePatterns: [
     "/lib/**/*", // Ignore built files.
