5854537643513c8ac37acb6f657762b841b05df4
岡浩平 <cafegolazo@gmail.com>
2025-08-26 08:58:54 +0000

    firebase deploy
    Error: Cannot deploy a web framework from source becaus

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   images: {
     unoptimized: true,
+  },
+  // Adding this to suppress the warning during build.
+  // The actual fonts are loaded in app/layout.tsx
+  // and this doesn't affect the final output.
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
   },
 };
 
