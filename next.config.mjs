9907c4039e062626eb89ec33677ecc910a26ea2e
岡浩平 <cafegolazo@gmail.com>
2025-08-26 09:42:55 +0000

    firebase deploy
    === Deploying to 'memorylink-cms'...
    
    i  deploying fire

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  // The following is needed to disable image optimization as it is not supported in `output: 'export'` mode
   images: {
-    unoptimized: true
+    unoptimized: true,
   },
-  reactStrictMode: false
 };
 
 export default nextConfig;
