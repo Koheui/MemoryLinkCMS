cb00916546a9e292ac4c84ed6207d2403feac137
岡浩平 <cafegolazo@gmail.com>
2025-08-26 10:27:49 +0000

    一回これを挟むというお決まりのパターンになってきましたね
     firebase deploy
    
    Error: Cannot deploy a w

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
+
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  publicRuntimeConfig: {
-    firebaseApiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    firebaseAuthDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-    firebaseProjectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    firebaseStorageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-    firebaseMessagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-    firebaseAppId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  },
+  output: 'export',
 };
 
 module.exports = config;
