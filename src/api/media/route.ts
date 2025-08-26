ad586fc1fcbc2519778c5d2048ca466482b28f18
岡浩平 <cafegolazo@gmail.com>
2025-08-26 02:05:42 +0000

    firebase deploy
    === Deploying to 'memorylink-cms'...
    
    i  deploying fire

--- src/api/media/route.ts
+++
@@ -1,2 +0,0 @@
-// This file is obsolete and has been removed to resolve build errors.
-// All API routes are now centralized in the /src/app/api directory.
--- src/api/memories/create/route.ts
+++
@@ -1,2 +0,0 @@
-// This file is obsolete and has been removed to resolve build errors.
-// The correct implementation is located at /src/app/api/memories/create/route.ts
