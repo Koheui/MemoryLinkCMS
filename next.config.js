06b076430f75bbbba11bf7afcefc71f61d1b786c
岡浩平 <cafegolazo@gmail.com>
2025-08-27 20:21:46 +0000

    これ、デプロイ先が違うとこになってしまう問題と重なっていますか？

--- next.config.js
+++ next.config.js
@@ -11,19 +11,6 @@ const nextConfig = {
         "fs": false,
       };
     }
-    // A simple, non-disruptive modification to bust cache.
-    config.optimization.minimizer = config.optimization.minimizer.map((plugin) => {
-        if (plugin.constructor.name === 'TerserPlugin') {
-            // @ts-ignore
-            plugin.options.terserOptions.output = {
-                // @ts-ignore
-                ...plugin.options.terserOptions.output,
-                // Add a unique comment to force cache invalidation on each build
-                preamble: `/* build-timestamp: ${Date.now()} */`,
-            };
-        }
-        return plugin;
-    });
     return config;
   },
 };
